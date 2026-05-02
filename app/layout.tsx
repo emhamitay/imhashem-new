8e665fe753e6ea7f0d08e713c7bcc06c1dd58a21
emhamitay <emhamitay@gmail.com>
2026-05-02 23:52:33 +0300

    tested link component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import { Link } from "../framework/Link"
 
 function Layout({children} : {children:React.ReactNode}) {
   return (
     <><div>This is a big header</div>
+    <Link href='/'>Home</Link>
+    <Link href='/login'>Login</Link>
       {children}
     </>
   );
